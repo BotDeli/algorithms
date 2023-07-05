16ca51ddfd4caedd125a4d80da3fead752f4b537
BotDeli <124913435+BotDeli@users.noreply.github.com>
2023-07-05 18:13:40 +0500

    bmh

---
+++ findMaxLengthPrefix/findMaxPrefix.go
@@ -0,0 +1,30 @@
+package findMaxLengthPrefix
+
+func findMaxLengthPrefix(str string) int {
+	if len(str) < 2 {
+		return 0
+	}
+	lengths := getArrayMaxLengthPrefix(str)
+	return lengths[len(str)-1]
+}
+
+func getArrayMaxLengthPrefix(str string) []int {
+	lengths := make([]int, len(str))
+	lengths[0] = 0
+	left, right := 0, 1
+	for left < len(str) && right < len(str) {
+		if str[left] == str[right] {
+			lengths[right] = left + 1
+			right++
+			left++
+		} else {
+			if left == 0 {
+				lengths[right] = 0
+				right++
+			} else {
+				left = lengths[left-1]
+			}
+		}
+	}
+	return lengths
+}
---
+++ findMaxLengthPrefix/findMaxPrefix_test.go
@@ -0,0 +1,59 @@
+package findMaxLengthPrefix
+
+import "testing"
+
+func TestOneChar(t *testing.T) {
+	maxLength := findMaxLengthPrefix("a")
+	if maxLength != 0 {
+		t.Errorf("expected maxLength to be 0, got %d", maxLength)
+	}
+}
+
+func TestDontEqualsTwoChar(t *testing.T) {
+	maxLength := findMaxLengthPrefix("ab")
+	if maxLength != 0 {
+		t.Errorf("expected maxLength to be 0, got %d", maxLength)
+	}
+}
+
+func TestEqualsTwoChar(t *testing.T) {
+	maxLength := findMaxLengthPrefix("aa")
+	if maxLength != 1 {
+		t.Errorf("expected maxLength to be 1, got %d", maxLength)
+	}
+}
+
+func TestThreeChar(t *testing.T) {
+	maxLength := findMaxLengthPrefix("aba")
+	if maxLength != 1 {
+		t.Errorf("expected maxLength to be 1, got %d", maxLength)
+	}
+}
+
+func TestFourChar(t *testing.T) {
+	maxLength := findMaxLengthPrefix("abab")
+	if maxLength != 2 {
+		t.Errorf("expected maxLength to be 2, got %d", maxLength)
+	}
+}
+
+func TestFiveChar(t *testing.T) {
+	maxLength := findMaxLengthPrefix("ababa")
+	if maxLength != 3 {
+		t.Errorf("expected maxLength to be 3, got %d", maxLength)
+	}
+}
+
+func TestNineChar(t *testing.T) {
+	maxLength := findMaxLengthPrefix("abbaabba")
+	if maxLength != 4 {
+		t.Errorf("expected maxLength to be 4, got %d", maxLength)
+	}
+}
+
+func TestLongStr(t *testing.T) {
+	maxLength := findMaxLengthPrefix("abccdeujbkabccdeuj")
+	if maxLength != 8 {
+		t.Errorf("expected maxLength to be 8, got %d", maxLength)
+	}
+}
