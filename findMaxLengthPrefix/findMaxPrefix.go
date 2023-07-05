8f8fb3163df8dc4c2f44b7002cc1c4e3efddbc2c
BotDeli <124913435+BotDeli@users.noreply.github.com>
2023-07-05 18:15:55 +0500

    bmh

--- findMaxLengthPrefix/findMaxPrefix.go
+++ findMaxLengthPrefix/findMaxPrefix.go
@@ -10,20 +10,17 @@ func findMaxLengthPrefix(str string) int {
 
 func getArrayMaxLengthPrefix(str string) []int {
 	lengths := make([]int, len(str))
-	lengths[0] = 0
 	left, right := 0, 1
-	for left < len(str) && right < len(str) {
+	for right < len(str) {
 		if str[left] == str[right] {
 			lengths[right] = left + 1
 			right++
 			left++
+		} else if left == 0 {
+			lengths[right] = 0
+			right++
 		} else {
-			if left == 0 {
-				lengths[right] = 0
-				right++
-			} else {
-				left = lengths[left-1]
-			}
+			left = lengths[left-1]
 		}
 	}
 	return lengths
